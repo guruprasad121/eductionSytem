8994fd433d473846d62e5af224a148797fd4c7b4
guruprasad121 <47474774+guruprasad121@users.noreply.github.com>
2021-11-03 09:26:58 +0530

    creating new education system project for

---
+++ eductionSytem/src/main/java/com/example/eductionSytem/EductionSytemApplication.java
@@ -0,0 +1,13 @@
+package com.example.eductionSytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EductionSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EductionSytemApplication.class, args);
+	}
+
+}
---
+++ eductionSytem/src/test/java/com/example/eductionSytem/EductionSytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eductionSytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EductionSytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
